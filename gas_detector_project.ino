0ff92a625356d9b09f987f66417256a7614e7f04
Sabrina Mostafa <78508655+sabrina-mostafa@users.noreply.github.com>
2022-12-11 01:41:23 +0600

    Add files via upload

---
+++ gas_detector_project.ino
@@ -0,0 +1,37 @@
+int led_pin=2 ;
+int Buzzer=4 ;
+int analog_pin=A0 ;
+int fan=A1 ;
+
+void setup() {
+  Serial.begin(9600);
+   pinMode(led_pin, OUTPUT);
+   pinMode(Buzzer, OUTPUT);
+   pinMode(analog_pin,INPUT);
+   pinMode(fan,OUTPUT);
+}
+
+void loop() {
+  int conc ;                     // concentation(ppm)
+  conc=analogRead(analog_pin);
+   //Serial.println(temp);
+  if(conc>270)
+  {
+    Serial.println("Gas Detection!!");  
+    digitalWrite(led_pin,HIGH);
+    digitalWrite(Buzzer,HIGH);
+    digitalWrite(fan,HIGH);
+    //delay(1000);
+    //digitalWrite(led_pin,LOW);
+    // digitalWrite(Buzzer,LOW);
+    //delay(1000);
+   }
+   else
+   {
+      Serial.println("No dangerous!"); 
+      digitalWrite(led_pin,LOW);
+      digitalWrite(Buzzer,LOW);
+      digitalWrite(fan,LOW);
+      //delay(500);
+   }
+}
